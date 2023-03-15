dd707c69b5e97dbbdfe4845e4a46521c7f987361
Abhyanshu Pandey <abhyanshuapandeyji@gmail.com>
2023-03-15 13:34:21 +0530

    Error fixed in the storage

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const storage = require('node-persist');
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
-const jsonbody = bodyparser.json();
+app.use(express.json());
 
 //initialize node persist
 storage.init();
 
 // to add new students
-app.post('/student', jsonbody, async (req, res) => {
+app.post('/student', async(req, res) => {
 
-    const students = await storage.getItem("students");
+    const students = await storage.getItem("students")||[];
     const newStud = req.body;
-    newStud.id = students ? students.length + 1 : 101;
+    newStud.id = students ? students.length + 101 : 101;
     await storage.setItem("students", [...students, newStud]);
     res.send(students);
 })
@@ -21,15 +20,15 @@ app.post('/student', jsonbody, async (req, res) => {
 // to get all students
 app.get("/student",async(req,res)=>{
     const students = await storage.getItem("students");
-    //res.send(employees);
-    
-    let html=`<h1>Employee data</h1>
+
+
+    let html=`<h1> All Students Data !</h1>
         <table>
             <thead>
                 <tr>
-                    <th>Student Id :</th>
-                    <th>Student Name :</th>
-                    <th>GPA :</th>
+                    <th><b>Student Id :<b>&nbsp</th>
+                    <th><b>Student Name :<b>&nbsp</th>
+                    <th><b>GPA :<b>&nbsp</th>
                 </tr>
             </thead>
     `
@@ -51,22 +50,23 @@ app.get("/student",async(req,res)=>{
 // to get a specific student
 app.get("/student/:id",async(req,res)=>{
     const students = await storage.getItem("students");
-    const id= req.params.id;
+
+    const id = req.params.id; 
     res.send(students.filter(x=>x.id==id));
+    
 });
 
 // to get a student based on gpa
 app.get("/bestgpa",async(req,res)=>{
     const students = await storage.getItem("students");
-    let bestgpa = students.filter(function(ele){
-        return ele.Math.max(req.params.gpa);
-    })
+    let bestgpa = students[0];
+    for(let i = 0; i < students.length ; i++){
+        if( bestgpa.gpa < students[i].gpa ){
+            bestgpa = students[i];
+        }
+    }
+    res.send( "<h1> Student Details <h1>" + "<br>" + "<b>Student Id:<b>" + bestgpa.id + "<br>" +  "<b>Student Name :<b>" + bestgpa.name + "<br>" + "<b>GPA: <b>" + bestgpa.gpa );
 
-    res.send("<h1>Highest Grade Student<h1>");
-    res.send("Student Id :",bestgpa.id);
-    res.send("Student Name:",bestgpa.name);
-    res.send("GPA : ", bestgpa.gpa);
-    
 });
 
 
